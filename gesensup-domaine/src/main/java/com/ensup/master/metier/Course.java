8de54fd6eeca0aafe34be537430afc66e4fe19ba
zeushias <zeushias89@gmail.com>
2020-07-06 14:58:26 +0200

    Classes du domaine

--- gesensup-domaine/src/main/java/com/ensup/master/metier/Course.java
+++ gesensup-domaine/src/main/java/com/ensup/master/metier/Course.java
@@ -8,4 +8,34 @@ package com.ensup.master.metier;
  */
 public class Course {
 
+	private String themeCourse;
+	private int numberHours;
+	
+	public Course(String themeCourse, int numberHours) {
+		super();
+		this.themeCourse = themeCourse;
+		this.numberHours = numberHours;
+	}
+
+	public String getThemeCourse() {
+		return themeCourse;
+	}
+
+	public void setThemeCourse(String themeCourse) {
+		this.themeCourse = themeCourse;
+	}
+
+	public int getNumberHours() {
+		return numberHours;
+	}
+
+	public void setNumberHours(int numberHours) {
+		this.numberHours = numberHours;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [themeCourse=" + themeCourse + ", numberHours=" + numberHours + "]";
+	}
+	
 }
